28c3e8ae17672c7f419a9c0f4f99a966711eb4ca
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-13 20:51:20 +0600

    Add erc20 interface

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
 contract Counter {
     uint256 public number;
+
+    constructor(){
+
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
